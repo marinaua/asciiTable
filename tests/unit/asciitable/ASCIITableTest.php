6d9ca19b27c46aa5d225f87fe0cebe459593fd81
marina.shynkarenko <marina.shynkarenko@opteck.com>
2014-09-25 00:22:11 +0300

    unit test added

--- tests/unit/asciitable/ASCIITableTest.php
+++ tests/unit/asciitable/ASCIITableTest.php
@@ -1,19 +1,24 @@
 <?php
 
-class ASIITableTEST extends \PHPUnit_Framework_TestCase {
+class ASIITableTest extends \PHPUnit_Framework_TestCase {
 
+    /** @var  \ASCIITable\ASCIITable */
     protected $asciitable;
 
     protected function setUp()
     {
-
+        $this->asciitable = new \ASCIITable\ASCIITable();
     }
 
     /**
      * @test
      */
-    public function getDrawer(){
+    public function setGetDrawer(){
+        $mockAsciiDrawer = $this->getMock('\ASCIITable\Draw\ASCII\ASCIIDrawer');
+        $this->asciitable->setDrawer($mockAsciiDrawer );
+        $asciiDrawer = $this->asciitable->getDrawer();
 
+        $this->assertSame($mockAsciiDrawer,$asciiDrawer);
     }
 
 }
